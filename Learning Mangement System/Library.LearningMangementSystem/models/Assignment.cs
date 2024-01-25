6cc1078f35c597300fca40e3e95e6b50d4828f6f
Chris Laughlin <chrislfla@gmail.com>
2024-01-24 23:43:26 -0500

    Added the rest of the required classes Assignment, Countitem, cours and modulde

---
+++ Learning Mangement System/Library.LearningMangementSystem/models/Assignment.cs
@@ -0,0 +1,12 @@
+﻿namespace Library.LearningMangementSystem.models
+{
+    public class Assignment
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public double TotalAvailablePoints { get; set; }
+        public DateTime DueDate { get; set; }
+
+
+    }
+}
