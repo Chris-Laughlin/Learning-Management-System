3e45d869e0997116dfa14cbe0ca97d45c39bb7a7
Chris Laughlin <chrislfla@gmail.com>
2024-01-24 22:58:10 -0500

    Testing

---
+++ Learning Mangement System/Library.LearningMangementSystem/models/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace Library.LearningMangementSystem.models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public Dictionary<int, double> Grades { get; set; }
+
+        public char CLassification { get; set; }
+
+        public Person()
+        {
+            Grades = new Dictionary<int, double>();
+        }
+    }
+}
